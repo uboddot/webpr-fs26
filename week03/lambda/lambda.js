8b7f7a88d1d8270ed955f3d435b16e3c929ef2b2
urielBrawer <uriel.brawer@students.fhnw.ch>
2026-03-16 08:27:01 +0100

    after week 3

--- week03/lambda/lambda.js
+++ week03/lambda/lambda.js
@@ -1,5 +1,49 @@
+const id = x => x;
+const konst = x => y => x;
+const snd = x => y => y;
 
+const T = konst;
+const F = snd;
 
+const and = a => b => a (b) (a);
+const or = a => a (a);
+
+const Pair = x => y => selector => selector(x)(y);
+const firstname = konst;
+const lastname  = snd;
+
+const first = p => p(T);
+const second = p => p(F);
+const oneTwo = Pair(1)(2);
+
+// first(oneTwo) = 0; // 1
+const pairEq = a => b => first(a) === first(b) && second(a) === second(b);
+const pairPlus = a => b => Pair( first(a) + first(b) ) ( second(a) + second(b) );
+const pairMap = f => p => Pair(f(first(p))) (f(second(p)));
+
+const square = x => x * x;
+const result = pairMap(square)(oneTwo);
+const sum = pairMap(square)(oneTwo);
+
+// Q8:y
+const s1 = x => y => z => x;
+const s2 = x => y => z => y;
+const s3 = x => y => z => z;
+
+const triple = x => y => z => selector => selector(x)(y)(z);
+const a = t => t(s1);
+const b = t => t(s2);
+const c = t => t(s3);
+
+const bran = triple("Brendan");
+const raven = bran("Stark")(16);
+const oneTwoThree = triple("Brendan");
+// console.log("hulu ", a(raven) === "Brendan", a(raven));
+
+const Left = x => f => g => f(x);
+const Right = x => f => g => g(x);
+// const either = e => f => g => e (f) (g);
+const either = id;
 
 // ----- special -----
 
--- week03/lambda/lambdaTest.js
+++ week03/lambda/lambdaTest.js
@@ -5,33 +5,33 @@ const ok = [];
 
 
 // // id
-// ok.push( id(1) === 1 );
-// ok.push( id(id) === id );
+ok.push( id(1) === 1 );
+ok.push( id(id) === id );
 //
 // // konst
-// ok.push( konst(42)(0) === 42 );
-// ok.push( konst(42)(1) === 42 );
-// ok.push( konst(42)(null) === 42 );
+ok.push( konst(42)(0) === 42 );
+ok.push( konst(42)(1) === 42 );
+ok.push( konst(42)(null) === 42 );
 //
 // // kite
-// ok.push( snd(null)(42) === 42 );
+ok.push( snd(null)(42) === 42 );
 //
 // // true
 //
-// ok.push( T(1)(0) === 1 );
-// ok.push( F(1)(0) === 0 );
+ok.push( T(1)(0) === 1 );
+ok.push( F(1)(0) === 0 );
 //
 // // and
-// ok.push( and(F)(F) === F );
-// ok.push( and(T)(F) === F );
-// ok.push( and(F)(T) === F );
-// ok.push( and(T)(T) === T );
+ok.push( and(F)(F) === F );
+ok.push( and(T)(F) === F );
+ok.push( and(F)(T) === F );
+ok.push( and(T)(T) === T );
 //
 // // or
-// ok.push( or(F)(F) === F );
-// ok.push( or(T)(F) === T );
-// ok.push( or(F)(T) === T );
-// ok.push( or(T)(T) === T );
+ok.push( or(F)(F) === F );
+ok.push( or(T)(F) === T );
+ok.push( or(F)(T) === T );
+ok.push( or(T)(T) === T );
 
 // flip
 // flip(f)(x)(y) = f(y)(x)
@@ -42,9 +42,9 @@ const ok = [];
 //
 // // Pair
 //
-// const dierk = Pair("Dierk")("König"); // immutable
-// ok.push( dierk(firstname) === "Dierk");
-// ok.push( dierk(lastname)  === "König");
+const dierk = Pair("Dierk")("König"); // immutable
+ok.push( dierk(firstname) === "Dierk");
+ok.push( dierk(lastname)  === "König");
 //
 // const tdierk = Triple("Dierk")("König")(50); // immutable
 // ok.push( tdierk(tfirstname) === "Dierk");
@@ -71,16 +71,31 @@ const ok = [];
 //
 
 
-// const safeDiv = num => divisor =>
-//     divisor === 0
-//     ? Left("schlecht!")
-//     : Right(num / divisor);
-//
+
+
+const safeDiv = num => divisor =>
+    divisor === 0
+    ? Left("schlecht!")
+    : Right(num / divisor);
+
+const res = safeDiv(1)(0);
+// either(res)(msg => console.log(msg))(msg => console.log(msg))
+
+const eShow = x => x(id)(id);
+
+const x_ = Math.random();
+
+console.log(safeDiv(1)(0))
+console.log(eShow(safeDiv(1)(0)))
+
+console.log(eShow( safeDiv(1)(0)) === "schlecht!" && eShow(safeDiv(x_)(1)) === x_);
+
 // either( safeDiv(1)(0)  )
 //       ( x => console.error(x))
 //       ( x => console.log(x));
+// wird zu
+// safeDiv(1)(0) ( x => console.error(x)) ( x => console.log(x) );
 
-//
 // const [Cash, CreditCard, Invoice, PayPal, pay] = Choice(4);
 // const cash = Cash ();
 // const card = CreditCard ("0000-1111-2222-3333");
